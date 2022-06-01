d69eff8a3f2f7a3093984f27511b25f3fee27b8e
Vadim Sinitsa <sinitsavr@gmail.com>
2022-06-01 15:56:08 +0300

    sumTwoNumbers_func2.0

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
-// const getSumTwoNumber = function(){
- const userInputNum1 = prompt('Enter first number');
-  const userInputNum2 = prompt('Enter second numver');
-
- const checkUserInput1 = userInputNum1===null || userInputNum1==='' || isNaN(Number(userInputNum1));
-  
- const checkUserInput2 = userInputNum2===null || userInputNum2==='' || isNaN(Number(userInputNum2));
-
-//   if(checkUserInput1 || checkUserInput2){
-//     console.log('error')
-//   }else{
-//     console.log(Number(userInputNum1)+Number(userInputNum2))
-//   }
-// }
-
-// getSumTwoNumber()
+const isInputNotNumber = function(value){
+  return value===null || value==='' || isNaN(Number(value)) ;
+}  
 
 const getSumTwoNumber = function(number1, number2){
-  return Number(userInputNum1)+Number(userInputNum2);
-
+  return number1+number2;
+  if(isInputNotNumber(number1) || isInputNotNumber(number2)){
+    return false;
+  }
+  return Number(number1)+Number(number2);
 }
 
-
-if(checkUserInput1 || checkUserInput2){
-  console.log('error')
-}else{
-  console.log(getSumTwoNumber(Number(userInputNum1),Number(userInputNum2)))
+const logResultInput = function(){
+  const userInputNum1 = prompt('Enter first number');
+  const userInputNum2 = prompt('Enter second number');
+  if(isInputNotNumber(userInputNum1) || isInputNotNumber(userInputNum2)){
+  const result = getSumTwoNumber(userInputNum1,userInputNum2);
+  if(result===false){
+    console.log('error')
+  }else{
+    console.log(getSumTwoNumber(Number(userInputNum1),Number(userInputNum2)))
+    console.log(result)
+  }
 }
+}
+logResultInput();
