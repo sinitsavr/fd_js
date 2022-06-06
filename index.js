24a5781b14788df4f721c9035461c90e49a89208
Vadim Sinitsa <sinitsavr@gmail.com>
2022-06-06 16:00:55 +0300

    add pin with iteration

--- index.js
+++ index.js
@@ -1,26 +1,16 @@
-const isInputNotNumber = function(value){
-  return value===null || value==='' || isNaN(Number(value)) ;
-}  
+const PIN_Secret = "1111";
+const MAX_TRY = 3;
 
-const getSumTwoNumber = function(number1, number2){
-  return number1+number2;
-  if(isInputNotNumber(number1) || isInputNotNumber(number2)){
-    return false;
-  }
-  return Number(number1)+Number(number2);
+let count = MAX_TRY;
+while (true) {
+  const PIN = prompt("Enter PIN");
+  count--;
+    if (PIN === PIN_Secret) {
+  console.log("Thank you!");
+    break;
 }
-
-const logResultInput = function(){
-  const userInputNum1 = prompt('Enter first number');
-  const userInputNum2 = prompt('Enter second number');
-  if(isInputNotNumber(userInputNum1) || isInputNotNumber(userInputNum2)){
-  const result = getSumTwoNumber(userInputNum1,userInputNum2);
-  if(result===false){
-    console.log('error')
-  }else{
-    console.log(getSumTwoNumber(Number(userInputNum1),Number(userInputNum2)))
-    console.log(result)
-  }
+if(count===0){
+  break;
 }
+console.log('try again:', count);
 }
-logResultInput();
