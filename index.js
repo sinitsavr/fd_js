2cec9f9982dc1a3451c41a12a8c958556b9e62af
Vadim Sinitsa <sinitsavr@gmail.com>
2022-06-10 16:28:51 +0300

    getRandomnumbers

--- index.js
+++ index.js
@@ -1,22 +1,14 @@
-function Ladder (){
-  this.step = 0;
+function getRandomArbitrary(min, max) {
+
+  return Math.random() * (max - min) + min;
 }
-function LadderStep(){
-  this.up = function(){
-    this.step++;
-    return this;
-    };
-    this.down = function(){
-      this.step--;
-      return this;
-      };
-    this.showStep = function(){
-      return this.step;
-    }
-    };
-Ladder.prototype = new LadderStep()
-const myLadder1 = new Ladder();
-console.log(myLadder1.up());
+const arr1 = [];
+const amount = 10;
+const min=-10;
+const max=10;
   
 
-
+for(let i=0; i<amount; i++){
+arr1.push(Math.round(getRandomArbitrary(min, max)));
+}
+//
