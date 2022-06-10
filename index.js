67242346b2b202c40148311aa8acbbfa6af20b18
Vadim Sinitsa <sinitsavr@gmail.com>
2022-06-10 16:14:20 +0300

    ladder

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-function createFamily (){
-  const family = {};
-  const countMemberFamily = prompt('how much?');
-  for(let i=0; i<countMemberFamily; i++){
-    const name = prompt ('name?');
-    const role = prompt ('role?');
-    family[name] = role
-  }
-  return family
+function Ladder (){
+  this.step = 0;
 }
-console.log(createFamily())
+function LadderStep(){
+  this.up = function(){
+    this.step++;
+    return this;
+    };
+    this.down = function(){
+      this.step--;
+      return this;
+      };
+    this.showStep = function(){
+      return this.step;
+    }
+    };
+Ladder.prototype = new LadderStep()
+const myLadder1 = new Ladder();
+console.log(myLadder1.up());
+  
+
+
