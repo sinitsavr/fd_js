952e15b0619c3ec34f4b827f597cda68b01ca91d
Vadim Sinitsa <sinitsavr@gmail.com>
2022-06-13 11:04:15 +0300

    math array with filter

--- index.js
+++ index.js
@@ -11,4 +11,27 @@ const max=10;
 for(let i=0; i<amount; i++){
 arr1.push(Math.round(getRandomArbitrary(min, max)));
 }
-//
+console.log(arr1);
+const negArr = arr1.filter(isNegative);
+
+
+const posArr = arr1.filter(isPositive);
+
+function isPositive(element){
+  return element>0 || element===0;
+}
+function isNegative(element){
+  return element<0 || element===-0;
+}
+console.log(negArr);
+console.log(posArr);
+
+const oddArr = arr1.filter(isOdd);
+function isOdd(element){
+  return element%2===0;
+}
+const evenArr = arr1.filter(isEven);
+function isEven(element){
+  return element%2===1};
+  console.log(evenArr);
+  console.log(oddArr);
