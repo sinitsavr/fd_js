4b35f6535b700947b802127a6865bc7896689338
Vadim Sinitsa <sinitsavr@gmail.com>
2022-06-17 15:17:24 +0300

    class with inherit

--- index.js
+++ index.js
@@ -1,19 +1,5 @@
 "use strict";
 
-const f1 = function (...restArguments){
-  console.log(restArguments)
-}
-
-const f2 = (num1,...numbers)=>{
-  console.log(numbers)
-}
-
-
-f1(1,1,1,1)
-f2(2,2,2,2)
-
-const sumNum = (...num)=> num.reduce((acum,vol)=>acum+vol);
-
-sumNum(3,6,8,7);
-
+const str = 'to Be or NoT To bE'.toLowerCase;
+console.log(str());
 
